b5e286eda32d5c94bffa386984590186b750b39c
Foufa Mastafa <mastafa.foufa@hotmail.com>
2020-03-18 19:21:18 +0100

    test file update

--- src/test.py
+++ src/test.py
@@ -7,6 +7,8 @@ import os, sys
 assert os.environ.get('CORONA'), 'Please set the environment variable CORONA'
 CORONA = os.environ['CORONA']
 DATA_PATH = CORONA + "/data/"
-with open(DATA_PATH+"test.txt", 'wb') as f:
+with open(DATA_PATH+"test.txt", 'w') as f:
     txt = "hey toto, we are testing Github scheduler"
+    txt_bis = "hey tata, we are testing Github scheduler"
     f.write(txt + "\n")
+    f.write(txt_bis + "\n")
